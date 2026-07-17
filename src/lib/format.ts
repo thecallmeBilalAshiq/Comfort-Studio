15a55adf6f517cb797c5ddcb4eab07011990c349
thecallmeBilalAshiq <bashiq031@gmail.com>
2026-07-17 16:39:02 +0500

    Deleting

--- src/lib/format.ts
+++
@@ -1,7 +0,0 @@
-export function formatPrice(cents: number, currency = "GBP"): string {
-  return new Intl.NumberFormat("en-GB", {
-    style: "currency",
-    currency,
-    maximumFractionDigits: 0,
-  }).format(cents / 100);
-}
